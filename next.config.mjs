f23b86430a1c788e56a54082e01836e7a48f5dd5
Vinayak Sapa <vinsapa@gmail.com>
2026-02-27 23:19:31 +0530

    font config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/fonts/GlacialIndifference-Regular.otf",
+        headers: [
+          { key: "Content-Type", value: "font/otf" },
+          { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
+        ],
+      },
+      {
+        source: "/fonts/GlacialIndifference-Bold.otf",
+        headers: [
+          { key: "Content-Type", value: "font/otf" },
+          { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
